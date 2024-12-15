6ce54639ec8ba2a778eb4076dfab813af5c35e83
Kavyareddy1902 <kavyareddyinamadugu88@gmail.com>
2024-12-15 13:23:25 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,9 @@
+// Example: Scroll to sections smoothly when clicking navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
